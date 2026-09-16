e5ff36467a1406f7716f3a1945ef27181d39134e
rajat3855 <rajatbhatihd@gmail.com>
2026-09-16 19:38:06 +0530

    Update 26. Remove Duplicates from sorted Array.py

--- 26. Remove Duplicates from sorted Array.py
+++ 26. Remove Duplicates from sorted Array.py
@@ -6,6 +6,7 @@ class Solution:
             if nums[i] != nums[j]:
                 i = i + 1
                 nums[i] = nums[j]
+            
             j = j + 1
             
         return i + 1
